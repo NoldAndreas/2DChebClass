e308835a145fc461f3ddc67ec42880042caae865
Andreas Nold <andreasnold05@gmail.com>
2014-04-10 10:11:48 +0100

    added new plots for Symmetry Classsification - Case Ib

--- Computations/PDEs/SymmetryClassification_Ib.m
+++ Computations/PDEs/SymmetryClassification_Ib.m
@@ -11,13 +11,33 @@ function SymmetryClassification_Ib()
     ChangeDirData([dirData filesep 'SymmetryClassification'],'ORG');
 
     %% Parameters
-    lambda1 = 3;
-    lambda2 = 1;
+    lambda1 = 0;    lambda2 = 1;  omega   = -1i;    
     
+    C1 = 1;     C2 = 0;
+        
+    x0 = 0;     y0 = 0; 
+    AL = 1;     VL = 0; %Parameters for base flow U(y) = AL*y + VL
     
     %% Initialization
     %     
         
+    shapeBox = struct('y1Min',-5,'y1Max',5,'N',[80,50],...
+                      'y2Min',0.1,'y2Max',1);
+    plotBox  = shapeBox;
+    plotBox.N = [80,80];
+    BX        = Box(shapeBox);
+    Pts       = BX.ComputeAll(plotBox);        
+    
+    t = [0,0.5,1,1.5,2];
+    
+    for i = 1:length(t)
+        subplot(2,2,i);
+        p = Psi(Pts.y1_kv,Pts.y2_kv,t(i));
+        BX.doPlots(real(p),'contour',struct('clabel',true));    pbaspect([1 1 1]);  
+        %subplot(1,2,2);     BX.doPlots(imag(p),'contour',struct('clabel',true));    pbaspect([1 1 1]);
+    end
+    
+    %% ****************************************************************
     shapeBox = struct('y1Min',-5,'y1Max',5,'N',[50,70],...
                       'y2Min',0.0,'y2Max',10);
     plotBox  = shapeBox;
@@ -51,6 +71,16 @@ function SymmetryClassification_Ib()
         h(z==inf) = 0;        
         h(z==-inf) = 0;
     %    h(y==0) = 0;
+    end
+
+    function p = Psi(x,y,t)
+        z1   = lambda2*(x-x0-(y0*AL+VL)*t)./y;
+        eta = log(y-y0)-lambda2*AL*t;        
+        
+        z2  = (-1i*omega)^(1/3)*(1i/(4*omega) + 1i*omega*lambda2^2 - 1 - eta);
+        
+        p   = C1*exp(1i*lambda2*(z1+lambda1*AL*t)+eta/2.*(1-1i*eta*omega)).*airy(0,z2) + ...
+              C2*exp(1i*lambda2*(z1+lambda1*AL*t)+eta/2.*(1-1i*eta*omega)).*airy(2,z2);
     end
 end
 
