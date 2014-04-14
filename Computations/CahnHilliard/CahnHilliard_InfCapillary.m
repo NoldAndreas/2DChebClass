fb8759c3f96656ef080262a9a1a6b123f6cc001f
Andreas Nold <andreasnold05@gmail.com>
2014-04-14 18:07:03 +0100

    Cahn Hilliard in Infinite Capillary - included simple dynamics

--- Computations/CahnHilliard/CahnHilliard_InfCapillary.m
+++ Computations/CahnHilliard/CahnHilliard_InfCapillary.m
@@ -25,20 +25,24 @@ function CahnHilliard_InfCapillary()
     %% Parameters
     
     % Numerical Parameters    
-    PhysArea  = struct('y2Min',0,'y2Max',10,'N',[20,20],'L1',1);    
-    PlotArea  = struct('y2Min',0,'y2Max',10,'N1',80,...
-                       'y1Min',-5,'y1Max',5,'N2',80);                                       
+    PhysArea  = struct('y2Min',0,'y2Max',1,'N',[40,40],'L1',1);
+    PlotArea  = struct('y2Min',0,'y2Max',1,'N1',80,...
+                       'y1Min',-1.5,'y1Max',1,'N2',80);                                       
     M = PhysArea.N(1)*PhysArea.N(2);   
              
     % Physical Parameters
     mu_s  = 0;   % Chemical potential (saturation = 0)
-    Cn    = 1;   % Cahn number (=1,microscopic scaling)
+    Cn    = 0.4;   % Cahn number (=1,microscopic scaling)
     theta = pi/2; %Equilibrium contact angle
-    Ca    = 0.1;   % Capillary number
-    rho_m = 0.5; % = (rhoL+rhoV)/(rhoL-rhoV)
+    Ca    = 0.3;   % Capillary number
+    rho_m = 2; % = (rhoL+rhoV)/(rhoL-rhoV)
+    Uwall = 0.0001;
+    Re    = 1;
     
     etaRho  = 1;
-    zetaRho = 0;
+    zetaRho = 10;
+    
+    plotTimes = 0:0.1:20;
                   
     %% Initialization
     
@@ -50,11 +54,30 @@ function CahnHilliard_InfCapillary()
     IC.doPlots(rho_ic,'SC');
     
     
-    %% Dynamics
-    
-    x_ic    = fsolve(@f_dyn,[rho_ic ; zeros(2*M,1)]);    
-    
- 
+    %% Dynamics   
+    mM             = ones(M,1);
+    mM(Ind.top | Ind.bottom)  = 0;
+    opts           = odeset('RelTol',10^-8,'AbsTol',10^-8,...
+                            'Mass',diag(repmat(mM,3,1)));
+    x_ic           = [rho_ic ; zeros(2*M,1)];
+    [outTimes,X_t] =  ode15s(@dx_dt,plotTimes,x_ic,opts);        
+
+    X_t       = X_t';
+	rho_t     = X_t(1:M,:);    
+    flux_t    = X_t(M+1:end,:);
+    %x_iguess = [rho_ic ; Uwall*(Pts.y2_kv/max(Pts.y2_kv)) ; zeros(M,1)];   
+    %x        = fsolve(@f_dyn,x_iguess);    
+    for i = 1:length(outTimes)
+        subplot(1,2,1);
+        IC.doPlots(rho_t(:,i),'contour'); hold on;
+        IC.doPlotsFlux(flux_t(:,i)); title(['t=',num2str(outTimes(i)),' , max = ',num2str(max(abs(flux_t(:,i))))]); 
+        
+        subplot(1,2,2);
+        IC.doPlots(rho_t(:,i),'SC');
+        
+        pause(0.1);
+    end
+
     %% Functions    
 	function y = f_eq(rho_s)
         y            = GetExcessChemPotential(rho_s,0,mu_s); 
@@ -71,38 +94,29 @@ function CahnHilliard_InfCapillary()
 	function y = f_dyn(x)        
         x        = reshape(x,M,3);
         rho      = x(:,1);
-        u        = x(:,2);
-        v        = x(:,3);
+        %u        = x(:,2);
+        %v        = x(:,3);
         uv       = x(:,2:3);
         uv       = uv(:);
         rho2     = repmat(rho,2,1); 
 
         %Continuity
-        cont     =  - Diff.div*(uv.*rho2);
+        cont     =  - Diff.div*(uv.*(rho_m+rho2));
         
-        %Convective term:        
-        C        = [diag(u) diag(v)]*Diff.grad;
-        %CC       = blkdiag(C,C);        
-        %- CC*uv +...
+        % Momentum Equation
+        momChemPot  = -(rho2+rho_m).*( -2*(1-3*rho2.^2).*(Diff.grad*rho)/Cn  ...
+                                       - Cn*Diff.gradLap*rho);                 
+        mom         =  momChemPot + ...
+                        + Ca*(etaRho*(Diff.LapVec*uv) +...
+                              zetaRho*(Diff.gradDiv*uv));  
         
-        momChemPot  = -(rho2+rho_m).*(Diff.grad*GetExcessChemPotential(rho,0,mu_s)); 
-        %+ (Diff.gradLap*rho)*Cn/Cak;                            
-        
-        mom      =  momChemPot + ...
-                    + Ca*(etaRho*(Diff.LapVec*uv) + ...
-                    + zetaRho*(Diff.gradDiv*uv));  
-        
-        %Pressure Term = - grad(p)/rho 
-        %              = - 1/rho*d(rho^2 *df )/drho *grad(rho)/(Cn*Cak)
-        %              = - (1 - 6rho + 6rho^2)*grad(rho)/(Cn*Cak)                
-        rhoB     = rho(Ind.bottom);
-        rhoT     = rho(Ind.top);
-        
-        %Boundary Conditions for velocities                
-        mom([Ind.top;Ind.top])      = uv([Ind.top;Ind.top]) - [ones(sum(Ind.top),1);zeros(sum(Ind.top),1)];
+        %Boundary Conditions for velocities                                  
+        mom([Ind.top;Ind.top])      = uv([Ind.top;Ind.top]) - ...
+                              [Uwall*ones(sum(Ind.top),1);zeros(sum(Ind.top),1)];
         mom([Ind.bottom;Ind.bottom])= uv([Ind.bottom;Ind.bottom]);        
         
         %Boundary Conditions for density       
+        rhoB     = rho(Ind.bottom);     rhoT     = rho(Ind.top);      
         cont(Ind.bottom) = Cn*(Ind.normalBottom*(Diff.grad*rho)) + cos(theta)*(rhoB-1).*rhoB;
         cont(Ind.top)    = Cn*(Ind.normalTop*(Diff.grad*rho)) + cos(theta)*(rhoT-1).*rhoT;        
         
@@ -115,6 +129,47 @@ function CahnHilliard_InfCapillary()
 %        drhodt(Ind.bottom) = rho_s(Ind.bottom) - rho_ic(Ind.bottom);%Ind.normalBottom*(Diff.grad*rho_s);
              
         y = [cont ; mom];                 
+    end
+
+    function dxdt = dx_dt(t,x)
+        
+        x        = reshape(x,M,3);
+        rho      = x(:,1);
+        uv       = x(:,2:3);
+        uv       = uv(:);
+        rho2     = repmat(rho,2,1); 
+
+        %Continuity
+        drhodt   =  - Diff.div*(uv.*(rho2+rho_m));
+        
+        % Momentum balance (no nonlinear term)
+        duvdt     = -( -2*(1-3*rho2.^2).*(Diff.grad*rho)/Cn  ...
+                                       - Cn*Diff.gradLap*rho)/(Ca*Re) ...
+                        + 1/Re*(etaRho*(Diff.LapVec*uv) +...
+                              zetaRho*(Diff.gradDiv*uv))./(rho2+rho_m);
+        
+        %Nonlinear term
+        %C        = [diag(u) diag(v)]*Diff.grad;
+        %CC       = blkdiag(C,C);
+        %- CC*uv                
+                
+        %Boundary Conditions for velocities
+        duvdt([Ind.top;Ind.top])      = uv([Ind.top;Ind.top]);
+        duvdt([Ind.bottom;Ind.bottom])= uv([Ind.bottom;Ind.bottom]);
+        
+        %Boundary Conditions for density
+        rhoB     = rho(Ind.bottom);     rhoT     = rho(Ind.top);      
+        drhodt(Ind.bottom) = Cn*(Ind.normalBottom*(Diff.grad*rho)) + cos(theta)*(rhoB-1).*rhoB;
+        thetaUp = UpperTheta(t);
+        drhodt(Ind.top)    = Cn*(Ind.normalTop*(Diff.grad*rho)) + cos(thetaUp)*(rhoT-1).*rhoT;        
+        
+
+        %flux_dir           = Diff.grad*mu_s;
+        %drhodt(Ind.bound)  = Ind.normal*flux_dir;        
+%        drhodt(Ind.top)    = rho_s(Ind.top) - rho_ic(Ind.top);%Ind.normalTop*(Diff.grad*rho_s);
+%        drhodt(Ind.bottom) = rho_s(Ind.bottom) - rho_ic(Ind.bottom);%Ind.normalBottom*(Diff.grad*rho_s);
+             
+        dxdt = [drhodt ; duvdt];
     end
 
 
@@ -130,6 +185,22 @@ function CahnHilliard_InfCapillary()
 
     function z = InitialGuess()
         z = tanh( Pts.y1_kv/(2*Cn));
+        %z = ones(M,1);
+    end
+
+    function PlotX(x)
+        x        = reshape(x,M,3);
+        rhos      = x(:,1);
+        us        = x(:,2);
+        vs        = x(:,3);
+        
+        figure;
+        subplot(1,2,1);     IC.doPlots(rhos);
+        subplot(1,2,2);     IC.doPlotsFlux([us;vs]);
+    end
+
+    function th = UpperTheta(t)
+        th = max(90 + 10*(t/10),100)*pi/180;
     end
 
 end
