2f1a910c37ac5f3c29bc7336d029dab55ff53eef
Andreas Nold <andreasnold05@gmail.com>
2014-10-08 22:20:23 +0100

    binary fluid diffuse interface problem converging to correct solution for y2max is 20 and 40 times 30 collocation points

--- Computations/CahnHilliard/ShearWithInterfaceBinaryFluid.m
+++ Computations/CahnHilliard/ShearWithInterfaceBinaryFluid.m
@@ -7,7 +7,7 @@ function ShearWithInterfaceBinaryFluid()
     ChangeDirData([dirData filesep 'CahnHilliard_ShearedInterface' filesep 'BinaryFluid'],'ORG');
     %% Parameters    
     PhysArea = struct('N',[40,30],'y2Min',0,'y2Max',[],'L1',10,... %12,80,50
-                      'NBorder',200);
+                      'NBorder',40);
 
     PlotArea = struct('y1Min',-20,'y1Max',20,'N1',80,...
                       'y2Min',0,'y2Max',PhysArea.y2Max,'N2',80);   
@@ -23,7 +23,7 @@ function ShearWithInterfaceBinaryFluid()
                     
     config = v2struct(optsPhys,optsNum);   
                       
-    for y2M = 18%:2:20%22:2:30%15:2.5:30
+    for y2M = 20%:2:20%22:2:30%15:2.5:30
         
         config.optsNum.PhysArea.y2Max = y2M;
         config.optsNum.PlotArea.y2Max = y2M;
